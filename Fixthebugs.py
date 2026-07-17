2fc9c16758e314b49d8fd9ac6fb159a2e2529236
Aleena Azeem <aleenaazeem.mm@gmail.com>
2026-07-17 20:50:27 +0500

    Add files via upload

---
+++ Fixthebugs.py
@@ -0,0 +1,45 @@
+print("Welcome to our coding class!")
+print("Today we are learning to debug")
+print("Errors are just secret clues!") 
+print("Can you spot what is wrong here?")
+print("You are doing an amazing job!")
+print("Let's run this code together!")
+#This is a secret message for you to find!
+
+
+print("--- Daily Motivation for Tech Leaders ---")
+print("Believing you are a coder is the first step.")
+print("Success in coding is built on daily practice.")
+#print("Success in coding is built on daily practice.")
+
+
+
+
+
+
+
+print("Welcome to our coding class!")
+print("Today we are learning to debug")
+print("Errors are just secret clues!")
+print("Can you spot what is wrong here?")
+print("You are doing an amazing job!")
+print("Let's run this code together!")
+
+# Broken Script Challenge
+print("Starting the countdown...")
+print("3...")
+print("2...")
+print("1...")
+print("Blast off!")
+#print("Blast off!")
+
+
+
+
+
+
+
+
+
+
+
