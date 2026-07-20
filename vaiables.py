e13f2e06d19cfc497cdd0c0fa29653fe252f2c88
Aleena Azeem <aleenaazeem.mm@gmail.com>
2026-07-20 21:03:51 +0500

    Add files via upload

---
+++ vaiables.py
@@ -0,0 +1,114 @@
+name = "Aleena"
+marks =10
+rating = 4.5
+glasses = False
+
+print(name)
+print(glasses)
+print(rating)
+print(marks)
+
+print("Welcome to our coding class!")
+
+teacher_name = "Aleena"
+favorite_number = 7
+
+print(teacher_name)
+print(favorite_number)
+
+student_name = "Maaz"
+print(student_name)
+
+favorite_hobby = "Reading"
+print(favorite_hobby)
+
+name = "Ebadullah"
+total = 1000
+marks = 3.5
+day_time = False
+print(name)
+
+
+teacher_name = "Aleena"
+favorite_number = 7
+print(teacher_name)
+print(favorite_number)
+
+
+
+
+name = "Ebadullah"
+age = 11
+height = 4.3
+weight = 1000
+male = True
+print(name)
+print("name")
+print(weight)
+
+
+word = "Hello" #string
+marks = 100 #integer
+weight = 10.5 #float
+day_time = True #boolean
+
+print(word)
+print("word")
+
+print(day_time)
+
+
+print(type(day_time))
+print(type(weight))
+
+
+name = "Aleena"
+marks =10
+rating = 4.5
+glasses = True
+
+print("My name is" + str(name) + " and I have" + str(marks) + "marks and rating " + str(rating) + "I have glasses " + str(glasses ))
+
+
+name = "Maryam"
+age = 9
+marks = 10.5
+day_time = True
+
+print("name")
+print(name)
+print(marks)
+
+
+teacher_name = "Aleena"
+favorite_number = 7
+print(teacher_name)
+print(favorite_number)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
